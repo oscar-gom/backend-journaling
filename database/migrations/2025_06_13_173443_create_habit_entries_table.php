4c33a9c708f26dbf12f5e16b2bc7758ecbc4d736
Oscar Gomez <oscargomezsedas@gmail.com>
2025-06-13 19:36:20 +0200

    feat: Implement HabitEntry model, controller, and migration for habit tracking

---
+++ database/migrations/2025_06_13_173443_create_habit_entries_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('habit_entries', function (Blueprint $table) {
+            $table->id('habit_entry_id');
+            $table->boolean('done')->default(false);
+            $table->text('note')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('habit_entries');
+    }
+};
