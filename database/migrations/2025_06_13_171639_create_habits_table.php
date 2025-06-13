08927d1db8399a97d791327f5a0f8cf5403f1996
Oscar Gomez <oscargomezsedas@gmail.com>
2025-06-13 19:33:39 +0200

    feat: Create Habit model, controller, and migration for habit tracking functionality

---
+++ database/migrations/2025_06_13_171639_create_habits_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('habits', function (Blueprint $table) {
+            $table->id('habit_id');
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->enum('frequency_unit', ['daily', 'weekly', 'monthly', 'weekdays', 'weekends']);
+            $table->integer('frequency_value')->default(1);
+            $table->date('last_occurrence_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('habits');
+    }
+};
