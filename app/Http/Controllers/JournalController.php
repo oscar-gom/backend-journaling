285f89557ec646e3c3869a9f1cdacd7fc986595a
Oscar Gomez <oscargomezsedas@gmail.com>
2025-06-14 18:22:50 +0200

    feat: Implement Journal resource methods in JournalController

--- app/Http/Controllers/JournalController.php
+++ app/Http/Controllers/JournalController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Journal;
 use Illuminate\Http\Request;
 
+use function Pest\Laravel\json;
+
 class JournalController extends Controller
 {
     /**
@@ -12,15 +14,16 @@ class JournalController extends Controller
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+        $journals = Journal::all();
+        if ($journals->isEmpty()) {
+            return response()->json([
+                'message' => 'No journals found'
+            ], 404);
+        }
+        return response()->json([
+            'journals' => $journals,
+            'message' => 'Journals retrieved successfully'
+        ], 200);
     }
 
     /**
@@ -28,31 +31,43 @@ class JournalController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $journal = Journal::create($request->all());
+        return response()->json([
+            'journal' => $journal,
+            'message' => 'Journal created successfully'
+        ], 201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Journal $journal)
+    public function show($id)
     {
-        //
+        $journal = Journal::find($id);
+
+        if (!$journal) {
+            return response()->json([
+                'message' => 'Journal not found'
+            ], 404);
+        }
+
+        return response()->json([
+            'journal' => $journal,
+            'message' => 'Journal retrieved successfully'
+        ], 200);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Journal $journal)
-    {
-        //
-    }
 
     /**
      * Update the specified resource in storage.
      */
     public function update(Request $request, Journal $journal)
     {
-        //
+        $journal->update($request->all());
+        return response()->json([
+            'journal' => $journal,
+            'message' => 'Journal updated successfully'
+        ], 200);
     }
 
     /**
@@ -60,6 +75,9 @@ class JournalController extends Controller
      */
     public function destroy(Journal $journal)
     {
-        //
+        $journal->delete();
+        return response()->json([
+            'message' => 'Journal deleted successfully'
+        ], 200);
     }
 }
