b2b6cc1d15ca94c51ee3f6d62b863e9b646f0efb
Oscar Gomez <oscargomezsedas@gmail.com>
2025-06-14 19:50:03 +0200

    feat: Implement authentication functionality with registration, login, and logout methods

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Container\Attributes\Auth;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $fields = $request->validate([
+            'name' => 'required|string|max:52',
+            'email' => 'required|email|unique:users|confirmed',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        $user = User::create($fields);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token,
+            'message' => 'User registered successfully'
+        ], 201);
+    }
+    public function login(Request $request)
+    {
+        $fields = $request->validate([
+            'email' => 'required|email|exists:users',
+            'password' => 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if ($user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'message' => 'Invalid credentials'
+            ], 401);
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token,
+            'message' => 'User login successful'
+        ], 200);
+    }
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+        return response()->json([
+            'message' => 'User logged out successfully'
+        ], 200);
+    }
+}
