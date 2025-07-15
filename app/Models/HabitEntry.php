091e02d117bff7f8cd23b0cea923753995eb8b7c
Oscar Gomez <oscargomezsedas@gmail.com>
2025-07-15 12:36:37 +0200

    fix: Ensure primary key is explicitly defined in HabitEntry model

--- app/Models/HabitEntry.php
+++ app/Models/HabitEntry.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class HabitEntry extends Model
 {
+    protected $primaryKey = 'habit_entry_id';
+
     public function habit()
     {
         return $this->belongsTo(Habit::class);
